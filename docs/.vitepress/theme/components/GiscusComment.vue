d849a9bf18156d0f16369377ac66272817ca010e
XingJi-love <3968523679@qq.com>
2025-01-21 12:19:47 +0800

    添加giscus评论

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,27 @@
+<template>
+    <div style="margin-top: 24px">
+        <Giscus
+            id="R_kgDONs90Yg"
+            repo="XingJi-love/VitePress"
+            repoid="R_kgDOIN2Meg"
+            category="Announcements"
+            categoryid="DIC_kwDONs90Ys4CmMcK"
+            mapping="pathname"
+            term="Welcome to giscus!"
+            reactionsenabled="1"
+            emitmetadata="0"
+            inputposition="top"
+            loading="lazy"
+            :theme="isDark ? 'dark' : 'light'"
+            :key="route.path"
+        ></Giscus>
+    </div>
+</template>
+
+<script setup>
+import Giscus from '@giscus/vue';
+import { useRoute, useData } from 'vitepress';
+
+const route = useRoute();
+const { isDark } = useData();
+</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -27,8 +27,7 @@ import 'nprogress-v2/dist/index.css' // 进度条样式
 
 import FriendsLinks from './components/FriendsLinks.vue' // 友链组件
 
-import giscusTalk from 'vitepress-plugin-comment-with-giscus'
-import { useData, useRoute } from 'vitepress' // 评论组件
+import GiscusComment from './components/GiscusComment.vue' // 评论组件
 
 
 const playlist = [
@@ -106,8 +105,13 @@ export default {
     })
   },
 
+  Layout() {
+        return h(Theme.Layout, null, {
+            'doc-after': () => h(GiscusComment),
+        });
+  },
+
   setup() { // 图片放大功能初始化
-    const { frontmatter } = useData();
     const route = useRoute();
     const initZoom = () => {
       // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
@@ -119,30 +123,6 @@ export default {
     watch(
       () => route.path,
       () => nextTick(() => initZoom())
-    );
-
-    // giscus配置
-    // Obtain configuration from: https://giscus.app/
-    giscusTalk({
-        repo: 'XingJi-love/VitePress', //仓库
-        repoId: 'R_kgDONs90Yg', //仓库ID
-        category: 'Announcements', // 讨论分类
-        categoryId: 'DIC_kwDONs90Ys4CmMcK', //讨论分类ID
-        mapping: 'pathname', // default: `pathname`
-        inputPosition: 'top', // default: `top`
-        lang: 'en', // default: `zh-CN`
-        lightTheme: 'light', // default: `light`
-        darkTheme: 'transparent_dark', // default: `transparent_dark`
-        // ...
-    },
-    {
-      frontmatter, route
-      },
-      // Whether to activate the comment area on all pages.
-      // The default is true, which means enabled, this parameter can be ignored;
-      // If it is false, it means it is not enabled.
-      // You can use `comment: true` preface to enable it separately on the page.
-      true
     );
   },
   /*
